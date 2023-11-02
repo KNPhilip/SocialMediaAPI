81daf6be9a84d99e81679b2097d4cd44bb751306
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2023-11-02 09:45:16 +0100

    Add ResponseDto

---
+++ YoutubeAPI/Dtos/ResponseDto.cs
@@ -0,0 +1,19 @@
+namespace YoutubeAPI.Dtos
+{
+    public class ResponseDto<T>
+    {
+        public T? Data { get; set; }
+        public required bool Success { get; set; }
+        public string? Error { get; set; }
+        public required int? NewId { get; set; }
+
+        public static ResponseDto<T> SuccessResponse(T data) =>
+            new() { Data = data, Success = true, Error = null, NewId = null };
+
+        public static ResponseDto<T> ErrorResponse(string message) =>
+            new() { Success = false, Error = message, NewId = null };
+
+        public static ResponseDto<T> CreatedResponse(T data, int newId) =>
+            new() { Data = data, Success = true, Error = null, NewId = newId };
+    }
+}
