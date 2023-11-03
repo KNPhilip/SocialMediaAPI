7b1e20407c00e51a24c330e9831cd534d640e0c7
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2023-11-03 13:28:59 +0100

    Add user model

---
+++ YoutubeAPI/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace TweetAPI.Models
+{
+    public class User
+    {
+        public required int Id { get; set; }
+        public required string Username { get; set; }
+        public required string PasswordHash { get; set; }
+    }
+}
