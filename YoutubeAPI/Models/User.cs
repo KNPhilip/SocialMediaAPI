3c20026087dfcbc606a8c0c67989c6f111e63862
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2024-03-30 23:39:41 +0100

    Introduce encapsulation

--- YoutubeAPI/Models/User.cs
+++ YoutubeAPI/Models/User.cs
@@ -2,7 +2,37 @@
 
 public sealed class User
 {
-    public required int Id { get; set; }
-    public required string Username { get; set; }
-    public required string PasswordHash { get; set; }
+    private int id;
+    private string username = string.Empty;
+    private string passwordHash = string.Empty;
+
+    public required int Id 
+    {
+        get => id;
+        set
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(value, nameof(Id));
+            id = value;
+        }
+    }
+
+    public required string Username 
+    {
+        get => username;
+        set
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(value, nameof(Username));
+            username = value;
+        }
+    }
+
+    public required string PasswordHash 
+    {
+        get => passwordHash;
+        set
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(value, nameof(PasswordHash));
+            passwordHash = value;
+        }
+    }
 }
